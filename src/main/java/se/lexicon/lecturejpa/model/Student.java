e43662d98ce8b24b669008e8b7b4da5d621a0cfe
Fatemeh <f.khalleghi.777@gmail.com>
2025-01-19 21:17:44 +0100

    Student and Address

--- src/main/java/se/lexicon/lecturejpa/model/Student.java
+++ src/main/java/se/lexicon/lecturejpa/model/Student.java
@@ -1,97 +1,87 @@
 package se.lexicon.lecturejpa.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import java.time.LocalDate;
-
-@Entity( name = "students")
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+// https://thorben-janssen.com/key-jpa-hibernate-annotations/
+@Entity(name = "students")
 public class Student {
-
     @Id
-    //    @GeneratedValue(strategy = GenerationType.IDENTITY) // For Number Sequence
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // For Number Sequence
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
+    @Column(nullable = false,length = 100)
     private String firstName;
+    @Column(nullable = false,length = 100)
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String email;
-    private boolean is_active;
-    private LocalDate creatDate;
-
-
-    public Student(String id, String firstName, String lastName, String email, boolean is_active, LocalDate creatDate) {
+    @Column(name = "is_active")
+    private boolean active;
+    //    @Column(columnDefinition = "DEFAULT NOW()")
+    private LocalDateTime createDate;
+    @OneToOne
+    private Address address;
+    protected Student() {
+    }
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.active = true;
+        this.createDate = LocalDateTime.now();
+    }
+    public Student(String id, String firstName, String lastName, String email, boolean active, LocalDateTime createDate) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.is_active = is_active;
-        this.creatDate = creatDate;
+        this.active = active;
+        this.createDate = createDate;
     }
-
-    protected Student() {
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Student{");
-        sb.append("id='").append(id).append('\'');
-        sb.append(", firstName='").append(firstName).append('\'');
-        sb.append(", lastName='").append(lastName).append('\'');
-        sb.append(", email='").append(email).append('\'');
-        sb.append(", is_active=").append(is_active);
-        sb.append(", creatDate=").append(creatDate);
-        sb.append('}');
-        return sb.toString();
-    }
-
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public boolean isIs_active() {
-        return is_active;
+    public boolean isActive() {
+        return active;
     }
-
-    public void setIs_active(boolean is_active) {
-        this.is_active = is_active;
+    public void setActive(boolean active) {
+        this.active = active;
     }
-
-    public LocalDate getCreatDate() {
-        return creatDate;
+    public LocalDateTime getCreateDate() {
+        return createDate;
     }
-
-    public void setCreatDate(LocalDate creatDate) {
-        this.creatDate = creatDate;
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", active=" + active +
+                ", createDate=" + createDate +
+                '}';
     }
 }
